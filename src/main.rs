52737371cd7e971b691345efecd62d69f1f8def1
mgrru <ru@fufu.moe>
2024-07-08 14:30:12 +0800

    添加一个检测屏幕状态的拓展 添加一个接收玩家移动的系统 添加一个限制玩家移动范围的系统 添加一个生成敌人的系统

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
 use bevy::{prelude::*, window::PrimaryWindow};
+use bevy_screen_diagnostics::{
+    ScreenDiagnosticsPlugin, ScreenEntityDiagnosticsPlugin, ScreenFrameDiagnosticsPlugin,
+};
+use rand::Rng;
+
+const PLAYER_SIZE: f32 = 64.0;
+const PLAYER_SPEED: f32 = 500.0;
+
+const ENEMY_SIZE: f32 = 64.0;
+const NUMBER_OF_ENEMIES: usize = 20;
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_systems(Startup, spawn_camera)
-        .add_systems(Startup, spwan_player)
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "RU-GAME".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_plugins((
+            ScreenDiagnosticsPlugin::default(),
+            ScreenFrameDiagnosticsPlugin,
+            ScreenEntityDiagnosticsPlugin,
+        ))
+        .add_systems(Startup, (spawn_camera, spwan_player, spwan_enemies))
+        .add_systems(Update, (player_movement, confine_player_movement))
         .run();
 }
 
 #[derive(Component)]
 pub struct Player {}
+
+#[derive(Component)]
+pub struct Enemy {}
 
 pub fn spwan_player(
     mut commands: Commands,
@@ -28,16 +52,101 @@ pub fn spwan_player(
     ));
 }
 
-pub fn spawn_camera(
+pub fn spwan_enemies(
     mut commands: Commands,
     window_query: Query<&Window, With<PrimaryWindow>>,
+    asset_server: Res<AssetServer>,
 ) {
     let window = window_query.get_single().unwrap();
 
-    commands.spawn(
-        Camera2dBundle {
-            transform: Transform::from_xyz(window.width()/2.0, window.height()/2.0, 0.0),
-            ..default()
+    for _ in 0..NUMBER_OF_ENEMIES {
+        let mut rng = rand::thread_rng();
+
+        let half_enemy_size = ENEMY_SIZE / 2.0;
+        let x_min = 0.0 + half_enemy_size;
+        let x_max = window.width() - half_enemy_size;
+        let y_min = 0.0 + half_enemy_size;
+        let y_max = window.height() - half_enemy_size;
+
+        let rand_x = rng.gen_range(x_min..=x_max);
+        let rand_y = rng.gen_range(y_min..=y_max);
+
+        commands.spawn((
+            SpriteBundle {
+                transform: Transform::from_xyz(rand_x, rand_y, 0.0),
+                texture: asset_server.load("sprites/ball_red_large.png"),
+                ..default()
+            },
+            Enemy {},
+        ));
+    }
+}
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn player_movement(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut player_query: Query<&mut Transform, With<Player>>,
+    time: Res<Time>,
+) {
+    if let Ok(mut transform) = player_query.get_single_mut() {
+        let mut derection = Vec3::ZERO;
+
+        if keyboard_input.pressed(KeyCode::ArrowLeft) || keyboard_input.pressed(KeyCode::KeyA) {
+            derection += Vec3::new(-1.0, 0.0, 0.0)
         }
-    );
+        if keyboard_input.pressed(KeyCode::ArrowRight) || keyboard_input.pressed(KeyCode::KeyD) {
+            derection += Vec3::new(1.0, 0.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::ArrowUp) || keyboard_input.pressed(KeyCode::KeyW) {
+            derection += Vec3::new(0.0, 1.0, 0.0)
+        }
+        if keyboard_input.pressed(KeyCode::ArrowDown) || keyboard_input.pressed(KeyCode::KeyS) {
+            derection += Vec3::new(0.0, -1.0, 0.0)
+        }
+
+        if derection.length() > 0.0 {
+            derection = derection.normalize();
+        }
+
+        transform.translation += derection * PLAYER_SPEED * time.delta_seconds();
+    }
+}
+
+pub fn confine_player_movement(
+    mut player_query: Query<&mut Transform, With<Player>>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    if let Ok(mut player_transform) = player_query.get_single_mut() {
+        let window = window_query.get_single().unwrap();
+
+        let half_player_size = PLAYER_SIZE / 2.0;
+        let x_min = 0.0 + half_player_size;
+        let x_max = window.width() - half_player_size;
+        let y_min = 0.0 + half_player_size;
+        let y_max = window.height() - half_player_size;
+
+        let mut translation = player_transform.translation;
+
+        if translation.x < x_min {
+            translation.x = x_min
+        } else if translation.x > x_max {
+            translation.x = x_max
+        }
+
+        if translation.y < y_min {
+            translation.y = y_min
+        } else if translation.y > y_max {
+            translation.y = y_max
+        }
+
+        player_transform.translation = translation
+    }
 }
